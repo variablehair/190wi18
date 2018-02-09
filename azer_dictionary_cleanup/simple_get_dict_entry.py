d26b85bf209e1b8258a3d67f90a85d6be126ebea
Tom Li <variablehair@gmail.com>
2018-02-08 20:56:47 -0800

    filtered dictionary and words

---
+++ azer_dictionary_cleanup/simple_get_dict_entry.py
@@ -0,0 +1,12 @@
+import string
+
+with open("dict.txt", encoding="utf-8") as infile, open("dict_entries.txt", encoding="utf-8", mode="w") as outfile:
+    for line in infile:
+        try:
+            word = line.split(maxsplit=1)[0]
+            if word.isupper():
+                word = word.strip(string.punctuation + string.digits + '´')
+                if len(word) >= 3:
+                    outfile.write(word + "\n")
+        except IndexError:
+            continue
