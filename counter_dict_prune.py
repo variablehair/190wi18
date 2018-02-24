8ef32ad75d3c690a82218f6f26ac5a5f78513f49
Tom Li <variablehair@gmail.com>
2018-02-23 18:06:17 -0800

    changed requests to get all pages

---
+++ counter_dict_prune.py
@@ -0,0 +1,91 @@
+import json
+from pprint import pprint
+import xlsxwriter
+
+with open('azer_dictionary_cleanup/dict_entries.txt') as f:
+    dict_entries = f.readlines()
+
+exclude_left = ['məli', 'malı', 'mək', 'maq']
+
+dat_acc_case = ['a', 'ə']
+#, 'i', 'u', 'ü', 'ı'
+prev_word_exclude = ['in', 'un', 'ün', 'ın']
+end_of_sentence = ['.', '?', '!', '\n']
+# and -1 = n
+
+def process_dat_acc_case(token_list, results):
+	next_l_token = None
+	for l_token in token_list:
+		if l_token in end_of_sentence:
+			break
+		elif len(l_token) <= 2:
+			next_l_token = None
+			continue
+		elif next_l_token is None:
+			next_l_token = l_token
+			continue 
+
+		if next_l_token[-1] in dat_acc_case:
+			if next_l_token[-2] == 'n' and l_token[-2:] in prev_word_exclude:
+				next_l_token = None
+				continue
+			dict_dat_acc = results.get(word, {})
+			dict_dat_acc[next_l_token] = dict_dat_acc.get(next_l_token, 0) + 1
+			results[word] = dict_dat_acc
+
+f = open('data/task1.json')
+d = json.load(f)
+k = list(d.keys())
+
+workbook = xlsxwriter.Workbook('data_task2.xlsx')
+
+for key in k:
+	try:
+		l = d[key]['Lines']
+	except KeyError:
+		continue
+
+	results = dict()
+
+	for r in l:
+		try:
+			word = r['Kwic'][0]['str']
+			if word.endswith(key):
+				#print('excluding no following letter')
+				continue
+			left = [ldict['str'].split(' ') for ldict in r['Left']]
+			left = [string for sublist in left for string in sublist]
+			right = [rdict['str'].split(' ') for rdict in r['Right']]
+			for lw in left:
+				if any(x in lw for x in exclude_left):
+					#print('found excluded key')
+					raise ValueError
+			
+			left.reverse()
+
+			process_dat_acc_case(left, results)
+			process_dat_acc_case(right, results)	
+					
+		#except KeyError:
+		#	continue 
+		except ValueError:
+			continue
+
+	if not results:
+		with open('all_results_pruned.txt', mode='w', encoding='utf-8') as outfile:
+			outfile.write(key)
+		continue
+
+	worksheet = workbook.add_worksheet(key)
+	row = 0
+
+	for token in results:
+		sorted_dat_acc_dict = sorted(results[token], key=results[token].get, reverse=True)
+		for skey in sorted_dat_acc_dict:
+			worksheet.write(row, 0, token)
+			worksheet.write(row, 1, skey)
+			worksheet.write(row, 2, results[token][skey])
+			row += 1
+
+workbook.close()
+f.close()
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 from login import tom_username, tom_password
 import requests
-import pprint #for testing
 import json
 import urllib.parse
 import time
-# import argparse
-
-""" 
-parser = argparse.ArgumentParser()
-parser.add_argument('query')
-args = parser.parse_args()
-"""
 
 root_url = 'https://the.sketchengine.co.uk'
 base_url='%s/bonito/run.cgi/' % root_url
@@ -19,29 +11,69 @@ login_url = 'https://the.sketchengine.co.uk/login/'
 logindata = {'username' : tom_username, 'password' : tom_password,'submit' : 'ok'}
 
 qlist = []
-queries = open('task1inc.txt', encoding='utf-8')
+queries = open('task1clean.txt', encoding='utf-8')
 for line in queries:
     qlist.append(line.replace('\n', ''))
 
-results = {}
-for query in qlist:
-	fquery = str('q[word=".*' + query + '.*"]')
-	attrs = dict(corpname = 'preloaded/turkic_az', q = fquery, pagesize = '94267206', format = 'json')
+s = requests.Session()
+s.auth = (tom_username, tom_password)
+s.get(login_url)
+url = base_url + 'view'
 
-	s = requests.Session()
-	s.auth = (tom_username, tom_password)
-	s.get(login_url)
+#results = {}
+
+api_log = open('api_log.txt', encoding='utf-8', mode='w')
+
+for query in qlist:
+	pagenum = 1
+	fquery = str('q[word=".*' + query + '.*"]')
+	attrs = dict(corpname = 'preloaded/turkic_az', q = fquery, pagesize = '1000', format = 'json', async=0, fromp=pagenum)
+	curr_results = list()
+
 	r = s.post(login_url,data=logindata)
 
 	encoded_attrs=urllib.parse.urlencode(attrs)
-	url = base_url + 'view'
+
 	r = s.get(url, params = attrs)
-	json_obj = r.json()
-	results[query] = json_obj
+	curr_res_dict = r.json()
 
-	print(str(len(results)) + " complete.\n")
+	try:
+		e = (curr_res_dict['error'])
+		api_log.write('ERROR on ' + str(query) + ':\t' + str(e) + '\n')
+		continue
+	except KeyError:
+		pass
 
-	time.sleep(20)
+	curr_results = curr_res_dict['Lines']
+	total_pages = curr_res_dict['numofpages']
+	api_log.write(query + ' page ' + str(pagenum) + 'done, len = ' + str(len(curr_results)) + '\n')			
+	
+	if total_pages > 1:
+		while pagenum <= total_pages:
+			pagenum += 1
+			attrs['fromp'] = pagenum
+			r = s.get(url, params=attrs)
+			curr_res_dict = r.json()
+			curr_results = curr_results + curr_res_dict['Lines']
+			api_log.write(query + ' page ' + str(pagenum) + 'done, len = ' + str(len(curr_results)) + '\n')			
+			time.sleep(20)
+			if pagenum % 25 == 0:
+				time.sleep(120)
+				print(str(pagenum))
+			if pagenum % 200 == 0 or pagenum == total_pages:
+				with open('data/temp/' + query + str(pagenum) +'.json', mode='w', encoding='utf=8') as f:
+					json.dump(curr_results, f)
+				curr_results = []
 
-with open('data/task1j.json', mode='w', encoding='utf-8') as outfile:
-	json.dump(results, outfile)
+	with open('data/temp/' + query + str(pagenum) +'.json', mode='w', encoding='utf=8') as f:
+		json.dump(curr_results, f)
+
+#	api_log.write('FINISHED ' + query + str(len(results)) + '\n')			
+#	print(str(len(results)) + '\n')
+
+	time.sleep(1200)
+
+#with open('data/task1j.json', mode='w', encoding='utf-8') as outfile:
+#	json.dump(results, outfile)
+
+api_log.close()
